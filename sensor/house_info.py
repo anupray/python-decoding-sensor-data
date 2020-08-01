712098069ec6afde47f8de18b7b28b3e94b55a45
Anup Kumar Ray <anup.ray@salesforce.com>
2020-08-01 18:57:04 +0530

    House Data Class

---
+++ sensor/house_info.py
@@ -0,0 +1,25 @@
+import datetime
+
+current_date = datetime.datetime.today()
+
+class HouseInfo(object):
+
+    def __init__(self, data):
+        self.data = data
+    
+    def get_data_by_area(self, field, rec_area=0):
+        field_data = []
+        for record in self.data:
+            if not rec_area:
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):
+                field_data.append(record[field])
+        return field_data
+    
+    def get_data_by_date(self, field, rec_date=current_date):
+        field_data = []
+        for record in self.data:
+            if record['date'] == rec_date.strftime("%m/%d/%y"):
+                field_data.append(record[field])
+        return field_data
+                
